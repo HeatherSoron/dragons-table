588dcfc92aeefadfad9bf22428c16d3e7d140178
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2014-09-30 22:16:48 +1300

    Add a toggle button for debug mode

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,5 @@
+var DEBUG_MODE = true;
+
 function init() {
 	log("Application initialized");
 }
@@ -8,4 +10,15 @@ function log(msg) {
 	// assumed to be a <pre> tag.
 	var elem = document.getElementById("debug-log");
 	elem.innerHTML = msg + "\n" + elem.innerHTML;
+}
+
+function toggleDebugMode() {
+	log("Toggling debug. Current state: " + (DEBUG_MODE ? "on" : "off"));
+	DEBUG_MODE = !DEBUG_MODE;
+	
+	var display = DEBUG_MODE ? 'block' : 'none';
+	var elems = document.getElementsByClassName('debug');
+	for (var i = 0; i < elems.length; ++i) {
+		elems[i].style.display = display;
+	}
 }
