44f5d6790702e5baa86a6bc5f999960743bc1bc2
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2014-10-02 22:39:44 +1300

    Add mouse/touch events for debugging

--- www/js/main.js
+++ www/js/main.js
@@ -14,6 +14,18 @@ function log(msg) {
 	elem.innerHTML = msg + "\n" + elem.innerHTML;
 }
 
+// taken from StackOverflow, because I don't feel like grabbing jQuery *yet*: http://stackoverflow.com/a/442474
+function getElemCoords(el) {
+	var _x = 0;
+	var _y = 0;
+	while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+		_x += el.offsetLeft - el.scrollLeft;
+		_y += el.offsetTop - el.scrollTop;
+		el = el.offsetParent;
+	}
+	return { top: _y, left: _x };
+}
+
 function toggleDebugMode() {
 	log("Toggling debug. Current state: " + (DEBUG_MODE ? "on" : "off"));
 	DEBUG_MODE = !DEBUG_MODE;
@@ -31,6 +43,16 @@ var app = {
 		this.map = new Map(this.canvas);
 		this.map.addObject(new Drawable(2, 1));
 		this.recalcMap();
+		
+		// note: Phonegap seems to send both mousedown and touchstart? Needs experimentation
+		this.canvas.addEventListener('mousedown', function(e) { log("mousedown"); app.handlePointerStart(e, app.canvas); } );
+		this.canvas.addEventListener('touchstart', function(e) { log("touchstart"); app.handlePointerStart(e.changedTouches[0], app.canvas); } );
+	},
+	
+	handlePointerStart: function(evt, elem) {
+		var coords = getElemCoords(elem);
+		log(evt.pageX - coords.left);
+		log(evt.pageY - coords.top);
 	},
 	
 	recalcMap: function() {
