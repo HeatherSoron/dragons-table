982bb47d7089412124956c9a9a2625673ecbbc98
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2014-10-04 11:28:49 +1300

    Set up client app files to be served by the server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var http = require('http').Server(app);
@@ -8,6 +9,9 @@ var state = {};
 
 app.use(bodyParser.json());
 app.use(cors());
+
+var parentDir = __dirname.substring(0, __dirname.lastIndexOf('/'));
+app.use('/play', express.static(parentDir + '/www'));
 
 app.get('/', function(req, res) {
 	res.send(JSON.stringify(state));
