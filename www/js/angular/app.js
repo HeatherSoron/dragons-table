d51624b87525461394484d31eca42602dcf497da
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2014-10-18 22:46:33 +1300

    Set up multi-select for subpage show/hide

--- www/js/angular/app.js
+++ www/js/angular/app.js
@@ -2,6 +2,12 @@ angular.module("DragonsTable", [])
 	.controller("MainCtrl", ['$scope', function($scope) {
 		log("Angular main controller initialized");
 		init();
+		
+		$scope.subpages = ['networking', 'map'];
+		$scope.shownSubpages = [];
+		$scope.show = function(item) {
+			return $scope.shownSubpages.indexOf(item) !== -1;
+		};
 	}])
 	.controller("MapCtrl", ['$scope', function($scope) {
 		log("Initializing map controller");
